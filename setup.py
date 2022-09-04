e42a67f8aa9e9811d5af217b8d0fd6622e65314e
thurn6101 <thurn6101@gmail.com>
2022-09-04 13:09:43 +0700

    Add two ints server&client Finished

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ setup(
             "Number_pub = my_py_pkg.Number_publisher:main",
             "counter_ = my_py_pkg.number_counter:main",
             "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
-            "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main" 
+            "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main",
+            "add_two_ints_client = my_py_pkg.add_two_ints_client:main" 
         ],
     },
 )
